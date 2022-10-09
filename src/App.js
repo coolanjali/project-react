1c80fcd4ce52e29417de715a370509a5bc97daef
coolanjali <112107778+coolanjali@users.noreply.github.com>
2022-10-09 17:37:13 +0200

    fd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Weather from "./Weather";
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <header className="App-header">
         <h1>Find the weather of your city🔎🔎</h1>
       </header>
+      <Weather/>
     </div>
   );
 }
